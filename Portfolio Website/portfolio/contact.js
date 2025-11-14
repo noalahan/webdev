ccce9d36c2ff3c6467e4f4a5f6b5e02b83a8ee68
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-11-13 16:17:59 -0800

    contact transitions

--- Portfolio Website/portfolio/contact.js
+++ Portfolio Website/portfolio/contact.js
@@ -31,24 +31,27 @@ $(document).ready(function () {
         info.setAttribute("href", row.link);
         info.style.transition = "color .5s";
         img.setAttribute("src", "image/logo.png");
-        img.style.transition = "display .5"
+        img.style.transition = "width .5s";
 
         let color = Math.floor(Math.random() * 3);
         container.style.backgroundColor = colors[color];
         container.style.transition = "color .5s";
 
-          // add hover effect
+        // add hover effect
         function reveal() {
-          img.style.display = "none";
-          container.style.color = color > 1 ? "white" : "black"
-          info.style.color = color > 1 ? "white" : "black"
+          img.style.transition = "width .25s";
+          img.style.width = "0px";
+          container.style.color = color > 1 ? "white" : "black";
+          info.style.color = color > 1 ? "white" : "black";
         }
-        function hide(){
+        function hide() {
           container.style.color = colors[color];
           info.style.color = colors[color];
+          img.style.transition = "width .5s";
+          img.style.width = "70px";
         }
         hide();
-        
+
         // using both click and hover for to be both phone and web friendly
         container.addEventListener("mouseover", reveal);
         container.addEventListener("click", reveal);
